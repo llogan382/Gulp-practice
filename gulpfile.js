4fae5b2362edd6b3f6a713af196741fda62fcfad
Luke Logan <llogan382@gmail.com>
2018-09-11 09:48:08 -0400

    Created an uglify function for the .js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 "use strict";
 var gulp = require('gulp'),
-
 // gulp-concat is added as one of the dependencies in package.json, 
 //and called here
-concat = require('gulp-concat');
+concat = require('gulp-concat'),
+uglify = require('gulp-uglify');
+
+
 
 //set the default tasks for gulp:
 gulp.task("concatScripts", function() {
@@ -14,12 +16,24 @@ gulp.src([
 //below is the source file.
 //pipe sends the data loaded to our concat task
 //concat taks a string, the name of the file
-.pipe(concat("app.js"))
+.pipe(concat('app.js'))
 //send the file to its destination
 //takes the data and writes it to disk
 //this is where we want our changes to end up, a directory in this case. 
 .pipe(gulp.dest("js"));
 });
+
+//create another gulp task.
+//create a function in the gulp-task, and an anonymous function
+//as the task callback where we do our work.  
+gulp.task("minifyScripts", function() {
+    //grab the source file, which is the result of the concat task. 
+    gulp.src("js/app.js")
+    //script gets piped directly to uglify method. 
+    .pipe(uglify())
+    //results of uglify go our .js file
+    .pipe(gulp.dest('js'));
+} );
 
 gulp.task("default", ["hello"], function() {
     console.log("This is the default task")
