36e7dda989bcf77b9fcbcc1572f0f6e173c40365
Luke Logan <llogan382@gmail.com>
2018-09-11 09:19:09 -0400

    Adds notes from the course

--- gulpfile.js
+++ gulpfile.js
@@ -12,11 +12,12 @@ gulp.src([
 'js/sticky/jquery.sticky.js', 
 'js/main.js'])
 //below is the source file.
-//this sends the data laded to our concat task
+//pipe sends the data loaded to our concat task
 //concat taks a string, the name of the file
 .pipe(concat("app.js"))
 //send the file to its destination
 //takes the data and writes it to disk
+//this is where we want our changes to end up, a directory in this case. 
 .pipe(gulp.dest("js"));
 });
 
